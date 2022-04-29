c0adeff1b40bade77a931291cfcdf4100d84df06
Andrew Moore <andrewdmoore84@gmail.com>
2022-04-29 00:28:39 -0400

    Refactor: rearrange code as well as file/folder structure

---
+++ controllers/notes.js
@@ -0,0 +1,73 @@
+const notesRouter = require('express').Router();
+const Note = require('../models/note');
+
+notesRouter.get('/', (request, response) => {
+  Note.find({}).then(notes => {
+    response.json(notes);
+  });
+});
+
+notesRouter.get('/:id', (request, response, next) => {
+  const id = request.params.id;
+
+  Note.findById(id)
+    .then(note => {
+      if (note) {
+        response.json(note);
+      } else {
+        response.statusMessage = `Note id#${id} not found`;
+        response.status(404).end();
+      }
+    })
+    .catch(error => next(error));
+});
+
+notesRouter.post('/', (request, response, next) => {
+  const noteInfo = request.body;
+
+  if (!noteInfo.content) {
+    return response.status(400).json({
+      error: 'content missing',
+    });
+  }
+
+  const newNote = new Note({
+    content: noteInfo.content,
+    important: noteInfo.important || false,
+    date: new Date(),
+  });
+
+  newNote.save()
+    .then(savedNote => {
+      response.json(savedNote);
+    })
+    .catch(error => next(error));
+});
+
+notesRouter.delete('/:id', (request, response, next) => {
+  const id = request.params.id;
+
+  Note.findByIdAndRemove(id)
+  // eslint-disable-next-line no-unused-vars
+    .then(_ => {
+      response.status(204).end();
+    })
+    .catch(error => next(error));
+});
+
+notesRouter.put('/:id', (request, response, next) => {
+  const id = request.params.id;
+  const { content, important } = request.body;
+
+  Note.findByIdAndUpdate(
+    id,
+    { content, important },
+    { new: true, runValidators: true, context:'query' }
+  )
+    .then(updatedNote => {
+      response.json(updatedNote);
+    })
+    .catch(error => next(error));
+});
+
+module.exports = notesRouter;
--- models/note.js
+++ models/note.js
@@ -1,19 +1,5 @@
 const mongoose = require('mongoose');
 
-// eslint-disable-next-line no-undef
-const url = process.env.MONGODB_URI;
-
-console.log('connecting to ', url);
-
-mongoose.connect(url)
-  // eslint-disable-next-line no-unused-vars
-  .then(_ => {
-    console.log('connected to DB');
-  })
-  .catch(error => {
-    console.log('error connecting to MongoDB: ', error.message);
-  });
-
 const noteSchema = new mongoose.Schema({
   content: {
     type: String,
