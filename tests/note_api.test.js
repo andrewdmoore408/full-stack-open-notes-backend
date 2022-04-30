69f34b1716b09df7b001d0a3d03bcd885376ee36
Andrew Moore <andrewdmoore84@gmail.com>
2022-04-29 23:49:26 -0400

    Add test to verify that a correctly formatted note is created and 201 is returned

--- tests/note_api.test.js
+++ tests/note_api.test.js
@@ -1,3 +1,5 @@
+/* global beforeEach afterAll test expect */
+
 const mongoose = require('mongoose');
 const supertest = require('supertest');
 const app = require('../app');
@@ -24,22 +26,22 @@ beforeEach(async () => {
   await noteObject.save();
 });
 
+afterAll(() => {
+  mongoose.connection.close();
+});
+
 test('notes are returned as json', async () => {
   await api
     .get('/api/notes')
     .expect(200)
     .expect('Content-Type', /application\/json/);
-});
-
-afterAll(() => {
-  mongoose.connection.close();
-});
+}, 10000);
 
 test('all notes are returned', async () => {
   const response = await api.get('/api/notes');
 
   expect(response.body).toHaveLength(initialNotes.length);
-});
+}, 10000);
 
 test('a specific note is within the required notes', async () => {
   const response = await api.get('/api/notes');
@@ -48,4 +50,39 @@ test('a specific note is within the required notes', async () => {
   expect(contents).toContain(
     'Browser can execute only Javascript'
   );
-});
+}, 10000);
+
+test('a valid note can be added', async () => {
+  const newNote = {
+    content: 'async/await simplifies making async calls',
+    important: true,
+  };
+
+  await api
+    .post('/api/notes')
+    .send(newNote)
+    .expect(201)
+    .expect('Content-Type', /application\/json/);
+
+  const response = await api.get('/api/notes');
+  const contents = response.body.map(r => r.content);
+
+  expect(response.body).toHaveLength(initialNotes.length + 1);
+  expect(contents).toContain('async/await simplifies making async calls');
+}, 10000);
+
+test('note without content is not created', async () => {
+  const faultyNote = {
+    content: '',
+    important: false,
+  };
+
+  await api
+    .post('/api/notes')
+    .send(faultyNote)
+    .expect(400)
+    .expect('Content-Type', /application\/json/);
+
+  const response = await api.get('/api/notes');
+  expect(response.body).toHaveLength(initialNotes.length);
+}, 10000);
