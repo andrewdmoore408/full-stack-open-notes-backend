3c93907c06bf0dc166ac872e5fc069a5c3de85a6
Andrew Moore <andrewdmoore84@gmail.com>
2022-04-30 00:50:35 -0400

    Update beforeEach function to fix asynchronous bug

--- tests/note_api.test.js
+++ tests/note_api.test.js
@@ -9,11 +9,13 @@ const Note = require('../models/note');
 
 beforeEach(async () => {
   await Note.deleteMany({});
-  let noteObject = new Note(helper.initialNotes[0]);
-  await noteObject.save();
+  console.log('cleared');
 
-  noteObject = new Note(helper.initialNotes[1]);
-  await noteObject.save();
+  const noteObjects = helper.initialNotes
+    .map(note => new Note(note));
+
+  const promiseArray = noteObjects.map(note => note.save());
+  await Promise.all(promiseArray);
 });
 
 afterAll(() => {
